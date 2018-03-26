32234882b39e0f1abccad6c14970ae52f686c5a0
Halil Murat Goksel <34871949+mgoks@users.noreply.github.com>
2018-03-26 11:58:43 -0400

    Update and rename 1-13.lower_case.c to 1-XX.lower_case.c

--- chapter1/1-XX.lower_case.c
+++ chapter1/1-XX.lower_case.c
@@ -1,4 +1,6 @@
-/* Write a program to convert its input to lowercase using a function lower(c)
+/* From the first edition:
+
+Write a program to convert its input to lowercase using a function lower(c)
 which returns c if c is not a letter and the lowercase value of c if it is a letter. */
 
 #include <stdio.h>
