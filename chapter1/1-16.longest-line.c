f6c55b1dcb2c505b208ac39ab3a44b91be28ef61
Halil Murat Göksel <34871949+mgoks@users.noreply.github.com>
2018-05-01 23:13:22 +0300

    fixed

--- chapter1/1-16.longest-line.c
+++ chapter1/1-16.longest-line.c
@@ -1,19 +1,13 @@
-/* TODO:
-
-Revise the main routine of the longest-line program 
+/* Revise the main routine of the longest-line program 
 so it will correctly print the length of arbitrarily long input lines, 
 and as much as possible of the text. */
 
 /* Note:
-It is worth mentioning in passing that even a program as small as this one presents some sticky
-design problems. For example, what should main do if it encounters a line which is bigger than
-its limit? getline works safely, in that it stops collecting when the array is full, even if no
-newline has been seen. By testing the length and the last character returned, main can
-determine whether the line was too long, and then cope as it wishes. In the interests of brevity,
-we have ignored this issue. */
+"By testing the length and the last character returned, main can determine whether the line was 
+too long, and then cope as it wishes. */
 
 #include <stdio.h>
-#define MAXLINE 10 /* maximum input line size */
+#define MAXLINE 1000 /* maximum input line size */
 
 int getLine(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -24,26 +18,30 @@ int main()
     int max;                /* max length seen so far */
     char line[MAXLINE];
     char longest[MAXLINE];  /* longest line, saved */
-    int c, nc;              /* character, number of characters */
+    int c;                  /* character*/
 
     max = 0;
-    while ((len = getLine(line, MAXLINE)) > 0)
+    while ((len = getLine(line, MAXLINE)) > 0){
+        if (len == MAXLINE-1 && line[MAXLINE-1] != '\n')
+            while ( (c = getchar()) != '\n' && c != EOF)
+                ++len;
         if (len > max){
             max = len;
             copy(longest, line);
         }
+    }
     if (max > 0){   /* there was a line */
-        printf("%s\n", longest);
+        printf("longest line: %s\nlength: %d\n", longest, max);
     }
 }
 
 /* getline: read a line into s, 
-return length if length <= MAXLINE-1, MAXLINE-1 otherwise*/
+return length if length <= MAXLINE-1, MAXLINE-1 otherwise */
 int getLine(char s[], int lim)
 {
     int c, i;
 
-    for (i=0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
     if (c == '\n'){
         s[i] = c;
